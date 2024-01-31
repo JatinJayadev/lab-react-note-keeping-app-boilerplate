3230b8ea041237a65bd217843a636a2bb5328ded
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-01-31 10:41:49 +0530

    Added

---
+++ pronote/src/App.jsx
@@ -0,0 +1,12 @@
+import Editor from './components/Editor'
+
+function App() {
+
+  return (
+    <div>
+      <Editor/>
+    </div>
+  )
+}
+
+export default App
---
+++ pronote/src/components/Editor.jsx
@@ -0,0 +1,37 @@
+import { Component } from 'react'
+import './Editor.css'
+
+class Editor extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            value: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <div>
+                    <h3>Input</h3>
+                    <textarea onChange={this.handleChange}></textarea>
+                </div>
+                <div>
+                    <h3>Pro Note</h3>
+                    <div className='valueContainer'>{this.state.value}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Editor;
